a84f36eea33a505c0f391a5672af192efd88a775
Pavlik474 <kadet1mkk@gmail.com>
2016-01-15 10:23:47 +0300

    LAB6K

---
+++ LAB6K/Unit1.dfm
@@ -0,0 +1,60 @@
+object Form1: TForm1
+  Left = 244
+  Top = 164
+  Width = 1017
+  Height = 550
+  Caption = 'Form1'
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'MS Sans Serif'
+  Font.Style = []
+  OldCreateOrder = False
+  PixelsPerInch = 96
+  TextHeight = 13
+  object Button1: TButton
+    Left = 56
+    Top = 16
+    Width = 145
+    Height = 81
+    Caption = #1055#1077#1088#1077#1080#1084#1077#1085#1086#1074#1072#1090#1100
+    TabOrder = 0
+    OnClick = Button1Click
+  end
+  object Button2: TButton
+    Left = 232
+    Top = 16
+    Width = 137
+    Height = 81
+    Caption = #1057#1074#1077#1088#1085#1091#1090#1100
+    TabOrder = 1
+    OnClick = Button2Click
+  end
+  object Button3: TButton
+    Left = 392
+    Top = 16
+    Width = 145
+    Height = 81
+    Caption = #1048#1085#1092#1086#1088#1084#1072#1094#1080#1103
+    TabOrder = 2
+    OnClick = Button3Click
+  end
+  object Button4: TButton
+    Left = 568
+    Top = 16
+    Width = 161
+    Height = 81
+    Caption = #1062#1074#1077#1090
+    TabOrder = 3
+    OnClick = Button4Click
+  end
+  object ListBox1: TListBox
+    Left = 64
+    Top = 144
+    Width = 737
+    Height = 265
+    ItemHeight = 13
+    TabOrder = 4
+  end
+end
---
+++ LAB6K/Unit1.pas
@@ -0,0 +1,114 @@
+unit Unit1;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls;
+
+type
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Button2: TButton;
+    Button3: TButton;
+    Button4: TButton;
+    ListBox1: TListBox;
+    procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+    procedure FormShow(Sender: TObject);
+    procedure Button3Click(Sender: TObject);
+    procedure Button4Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+function EnumProc (Wd: HWnd; Param: LongInt): Boolean; stdcall;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+if findwindow(nil, ('Компьютер')) <> 0 then
+  begin
+  showmessage('Компьютер найден');
+  setwindowtext(findwindow(nil, ('Компьютер')),'Царь папка');
+  showmessage('Компьютер переименован');
+  end;
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+begin
+if IsIconic( findwindow(nil,'Компьютер')) then begin
+    Form1.Button2.Caption := 'Развернуть';
+    OpenIcon(findwindow(nil,'Компьютер'));
+    Form1.Button2.Caption :='Свернуть';
+  end
+else begin
+  Form1.Button2.Caption :='Свернуть';
+  CloseWindow(findwindow(nil,'Компьютер'));
+  Form1.Button2.Caption := 'Развернуть';
+  end;
+end;
+
+procedure TForm1.FormShow(Sender: TObject);
+begin
+if findwindow(nil, ('Компьютер')) <>0 then begin
+    if IsIconic( findwindow(nil,'Компьютер') )
+    then Form1.Button2.Caption := ('Развернуть')
+    else Form1.Button2.Caption := ('Свернуть');
+    end
+  else  showmessage ('Компьютер не найден');
+end;
+
+function EnumProc (Wd: HWnd; Param: LongInt): Boolean; stdcall;
+Var
+  Nm: Array[0..255] of Char;
+  Cs: Array[0..255] of Char;
+Begin
+  GetWindowText(Wd,Nm,255);
+  GetClassName(Wd,Cs,255);
+  Form1.ListBox1.Items.Add(String(Nm)+' / '+String(Cs){+'/'+String(Gwt)});
+  EnumProc := TRUE;
+end;
+
+procedure TForm1.Button3Click(Sender: TObject);
+begin
+  Form1.ListBox1.Items.Clear;
+  EnumWindows(@EnumProc, 0);
+end;
+
+procedure TForm1.Button4Click(Sender: TObject);
+ var
+ hnd:thandle;
+ canvas:tcanvas;
+ dc:hdc;
+ begin
+ hnd:=findwindow(nil, ('Form1'));
+ hnd:=findwindowex(hnd,0,'Form1.ListBox1',0);
+ showwindow(hnd,sw_hide);
+ sendmessage(hnd,wm_paint,0,0);
+ sleep(1000);
+ showwindow(hnd,sw_show);
+ sendmessage(hnd,wm_paint,0,0);
+ sleep(1000);
+ canvas:=tcanvas.create;
+ dc:=getdc(hnd);
+ with canvas do
+ begin
+ handle:=dc;
+ rectangle(0,0,100,100);
+ font.color:=$0000ff;
+ textout(650,360,'Шеф');
+ textout(690,360,'Всё пропало');
+ free;
+ end;
+ releasedc(0,dc);
+end;
+
+end.
